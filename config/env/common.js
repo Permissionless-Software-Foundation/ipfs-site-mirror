89642943f5584a1d0a47fbe7dde1f1dd313f59db
Chris Troutner <chris.troutner@gmail.com>
2019-11-23 12:17:44 -0800

    Using blog example template and new memo account

--- config/env/common.js
+++ config/env/common.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`,
+  BCHADDR: `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
   stateFileName: `state.json`,
   ipfsPort1: 4002,
   ipfsPort2: 4003
