9c7de000ef4fcef07e81ca7fe1ebabc7b2ffdd97
Chris Troutner <chris.troutner@gmail.com>
2020-03-14 13:16:40 -0700

    fix(ipfs): Updated depenencies and fixed IPFS and BCH interfaces

--- src/lib/ipfs.js
+++ src/lib/ipfs.js
@@ -7,68 +7,85 @@ let ipfs
 const em = require('./utils/eventJS')
 
 async function startIPFS () {
-  // starting ipfs node
-  console.log('Starting...!')
-  const ipfsOptions = {
-    repo: './ipfs-data/ipfs-config/node',
-    start: true,
-    EXPERIMENTAL: {
-      pubsub: true
-    },
-    relay: {
-      enabled: true, // enable circuit relay dialer and listener
-      hop: {
-        enabled: true // enable circuit relay HOP (make this node a relay)
+  try {
+    // starting ipfs node
+    console.log('Starting...!')
+
+    const ipfsOptions = {
+      repo: './ipfs-data/ipfs-config/node',
+      start: true,
+      EXPERIMENTAL: {
+        pubsub: true
+      },
+      relay: {
+        enabled: true, // enable circuit relay dialer and listener
+        hop: {
+          enabled: true // enable circuit relay HOP (make this node a relay)
+        }
       }
     }
-  }
-  if (config.ipfsPort1 && typeof config.ipfsPort1 === 'number' &&
-  config.ipfsPort2 && typeof config.ipfsPort2 === 'number') {
-    // Adding ports to ipfs config
-    ipfsOptions.config = {
-      Addresses: {
-        Swarm: [
-          `/ip4/0.0.0.0/tcp/${config.ipfsPort1}`,
-          `/ip4/127.0.0.1/tcp/${config.ipfsPort2}/ws`
-        ],
-        API: `/ip4/127.0.0.1/tcp/${config.ipfsPort1}`,
-        Gateway: `/ip4/127.0.0.1/tcp/${config.ipfsPort2}`
+
+    if (
+      config.ipfsPort1 &&
+      typeof config.ipfsPort1 === 'number' &&
+      config.ipfsPort2 &&
+      typeof config.ipfsPort2 === 'number'
+    ) {
+      // Adding ports to ipfs config
+      ipfsOptions.config = {
+        Addresses: {
+          Swarm: [
+            `/ip4/0.0.0.0/tcp/${config.ipfsPort1}`,
+            `/ip4/127.0.0.1/tcp/${config.ipfsPort2}/ws`
+          ],
+          API: `/ip4/127.0.0.1/tcp/${config.ipfsPort1}`,
+          Gateway: `/ip4/127.0.0.1/tcp/${config.ipfsPort2}`
+        }
       }
     }
+
+    // instantiating  ipfs node
+    ipfs = await IPFS.create(ipfsOptions)
+    em.emit('test')
+    return ipfs
+  } catch (err) {
+    console.error(`Error in ipfs.js/startIPFS()`)
+    throw err
   }
-  // instantiating  ipfs node
-  ipfs = new IPFS(ipfsOptions)
-  em.emit('test')
-  return ipfs
 }
 
 // Get the latest content from the IPFS network and Add into ipfs-data.
 async function getContent (ipfsNode, hash) {
-  em.emit('download-start')
-  // Get the latest content from the IPFS network.
-  return new Promise((resolve, reject) => {
-    ipfsNode.get(hash, async function (err, files) {
-      if (err) {
-        em.emit('download-stop')
-        reject(err)
-      }
-
-      const pathStore = `${process.cwd()}/ipfs-data/` // Path to store new ipfs-data
-      files.forEach(async file => {
-        // Map files
-        //  console.log(file)
-        if (file.type === 'file') {
-          // Is File
-          fs.writeFileSync(`${pathStore}${file.path}`, file.content)
-        } else if (file.type === 'dir') {
-          // Is Folder
-          fs.mkdirSync(`${pathStore}${file.path}`, { recursive: true })
+  try {
+    em.emit('download-start')
+    // Get the latest content from the IPFS network.
+    return new Promise((resolve, reject) => {
+      ipfsNode.get(hash, async function (err, files) {
+        if (err) {
+          em.emit('download-stop')
+          reject(err)
         }
+
+        const pathStore = `${process.cwd()}/ipfs-data/` // Path to store new ipfs-data
+        files.forEach(async file => {
+          // Map files
+          //  console.log(file)
+          if (file.type === 'file') {
+            // Is File
+            fs.writeFileSync(`${pathStore}${file.path}`, file.content)
+          } else if (file.type === 'dir') {
+            // Is Folder
+            fs.mkdirSync(`${pathStore}${file.path}`, { recursive: true })
+          }
+        })
+        em.emit('download-stop')
+        resolve(true)
       })
-      em.emit('download-stop')
-      resolve(true)
     })
-  })
+  } catch (err) {
+    console.error(`Error in ipfs.js/getContent()`)
+    throw err
+  }
 }
 // Adds an IPFS object to the pinset and also stores it to the IPFS repo.
 
