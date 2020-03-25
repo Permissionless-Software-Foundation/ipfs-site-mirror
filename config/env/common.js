be8636e3837c141d0279d7a2472ade541b86bd5c
Chris Troutner <chris.troutner@gmail.com>
2020-03-25 09:20:34 -0700

    Adding commented link to troutsblog

--- config/env/common.js
+++ config/env/common.js
@@ -6,6 +6,7 @@
 module.exports = {
   port: process.env.PORT || 3210,
   BCHADDR: `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
+  // BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`, // troutsblog.com
   stateFileName: `state.json`,
   ipfsPort1: 4002,
   ipfsPort2: 4003
