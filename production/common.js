05ce9c459e4fe610c67c41bdef934f5fe3faa73e
Chris Troutner <chris.troutner@gmail.com>
2020-04-27 20:12:41 -0700

    Updating production docker container

--- production/common.js
+++ production/common.js
@@ -5,6 +5,9 @@
 
 module.exports = {
   port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qz5sv5ly2r379yqzpz3pwct66q8ph9cgnv3ku0w4qa`,
-  stateFileName: `state.json`
+  BCHADDR: `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
+  // BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`, // troutsblog.com
+  stateFileName: `state.json`,
+  ipfsPort1: 4002,
+  ipfsPort2: 4003
 }
