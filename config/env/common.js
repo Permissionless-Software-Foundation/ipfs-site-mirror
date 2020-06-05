426d43b0efecb1e6fc46b00a49c0bd4ae10d966e
Daniel Gonzalez <danielhumgon@gmail.com>
2020-06-05 04:07:54 -0400

    fix(download): fixed ipfs getContent() function

--- config/env/common.js
+++ config/env/common.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
+  BCHADDR: `bitcoincash:qpjkzcm3kgvu932w4lzj64ew4j4gqs3vxvt7ks55qc`,
   // BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`, // troutsblog.com
   stateFileName: `state.json`,
   ipfsPort1: 4002,
