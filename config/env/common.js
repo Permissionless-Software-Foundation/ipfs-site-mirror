f813c7666aa3f62add525eedfa587c4b2be4a03b
Chris Troutner <chris.troutner@gmail.com>
2021-01-24 18:27:19 -0800

    fix(config): Overriding other config settings with env vars

--- config/env/common.js
+++ config/env/common.js
@@ -5,9 +5,9 @@
 
 module.exports = {
   port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
+  BCHADDR: process.env.BCHADDR || `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
   // BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`, // troutsblog.com
   stateFileName: `state.json`,
-  ipfsPort1: 4002,
-  ipfsPort2: 4003
+  ipfsPort1: process.env.IPFS_PORT1 || 4002,
+  ipfsPort2: process.env.IPFS_PORT2 || 4003
 }
